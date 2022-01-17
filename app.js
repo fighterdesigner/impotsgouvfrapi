557a55209e8a1abb047a1e418a7d869d291542ba
fighterdesigner <designsleader@gmail.com>
2022-01-17 09:10:13 +0000

    add cors

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const puppeteer = require('puppeteer');
 const express = require('express');
 const bodyParser = require("body-parser");
 const app = express();
+const cors = require('cors');
+
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
